9e9716e5f6d3edef4a6ebc044a94cda60fe6e746
Mequam <blue9ja@gmail.com>
2019-08-19 12:47:57 -0400

    pulled the updated changes from master into safe

--- server/parser/parse.pm
+++ server/parser/parse.pm
@@ -4,22 +4,20 @@ use strict;
 use warnings;
 use 5.03.0;
 
-print "works\n";
 sub inP
 {
 #this is a subroutine to test wether or not a given string is surrounded
 #completly by a parenthasis pair of the callers choosing
-#the sub routine defaults to using '()'
+#the sub routine defaults to using '()'	
 	my ($str,$open,$close) = @_;
 	if (not defined $str)
 	{
 		print "[inP] ERROR: string argument required!\n";
 		die;
 	}
-	if (not defined $open)
-	{ $open = '(';}
-	if (not defined $close) {$close = ')'}
 
+	if (not defined $open) {$open = '(';}
+	if (not defined $close) {$close = ')'}	
 
 	my @arrStr = split '', $str;
 	my $parenthLevel = 0;
@@ -35,41 +33,128 @@ sub inP
 	if ($arrStr[scalar(@arrStr) - 1] eq $close) {return 1;}
 	else {return 0;}
 }
+sub stripOuter
+{	
+	my $str = shift;
+	while (inP($str))
+	{	
+		$str = substr($str,1,(length($str)-2));
+	}
+	return $str;
+}
 sub parseS
 {
-	my ($state,@opps,@funcs) = @_;
+	#set up the variables for the sub routine
+
+	#the array is probably not getting passed how we would want it to
+	my ($state,$oppsRef,$funcsRef,$open,$close) = @_;
+
+	#strip the given statement of any outside parenthasis
+	$state = stripOuter $state;
 	
-	print "$state\n";
+	#dereference the oppsRef to get acces to the varaibles stored within it	
+	my @oppsArr = @$oppsRef;
+	
+	#default the parenthasis if it is not given
+	if (not defined $open) {$open = '(';}
+	if (not defined $close) {$close = ')';}	
+
+	#set up the state varaible as somthing we can iterate over
 	my @stateArr = split '', $state;
-
-	for (my $i = 0; $i < scalar @stateArr;$i++)
+	
+	my $parenthLvl = 0;
+	
+	for (my $i = 0; $i < length $state;$i++)
 	{
-		for (my $j = 0; $j < scalar @opps;$j++)
-		{
-			if ($opps[$j][1] eq $stateArr[$i])
-			{
-				print "matched a $stateArr[$i]\n";
-			}
+	
+	#set the parenthasis level
+		if  ($stateArr[$i] eq $open)
+		{	
+			$parenthLvl += 1;
+			next;
 		}
-		$stateArr[$i];
-	}
-	print "\n";
+		elsif ($stateArr[$i] eq $close)
+		{	
+			$parenthLvl -= 1;
+			next;
+		}
+	#check the parenthasis lvl
+		
+		if ($parenthLvl == 0 and $stateArr[$i] ne $open and $stateArr[$i] ne $close)
+		{
+			#only check the letter if we know its not in parenthasis and is 
+			#not a parenthasy	
+			for (my $j = 0; $j < scalar @oppsArr;$j++)
+			{	
+				if ($oppsArr[$j][1] eq $stateArr[$i])
+				{	
+					#split the string up into two strings and return the function of 
+					#those two string + 1s
+					my $stringB = substr $state , $i + 1;
+					my $stringA = substr $state, 0, $i;		
+						
+					my $ret_val = &{$oppsArr[$j][0]}(parseS($stringA,$oppsRef,$funcsRef,$open,$close), parseS($stringB,$oppsRef,$funcsRef,$open,$close));							
+					return $ret_val;
+				}
+			}
+		}	
+		
+		
+	}	
+	#we have been unable to find any valid operations to check, this is where we store the functions that we want to parse
+	return int($state);
 }
-parseS("1+1",(["test\n","+"]));
-inP "fake string\n";
 
-my $ui = 'blah';
-while (not ($ui eq 'q'))
-{
-	print "(string)> ";
-	$ui = <STDIN>;
-	if (inP(substr($ui,0,length $ui)) == 1)
+
+
+my $addRef = sub 
+{	
+	my $ret_val = 0;
+	foreach (@_)
 	{
-		print "you are inside of parenthasis\n";
+		$ret_val += $_
 	}
-	else
+	return $ret_val;
+	
+};
+
+my $subRef = sub
+{
+	my ($x,$y) = @_;
+	return $x - $y; 
+};
+
+my $multRef = sub
+{
+	my $ret_val = 1;
+	foreach (@_)
 	{
-		print "you are not inside of parenthasis\n";
+		$ret_val *= $_;
+	}
+	return $ret_val;
+};
+my $divRef = sub
+{
+	my ($x,$y) = @_;
+	return $x/$y;
+};
+
+sub test 
+{
+	print inP "test";
+	print inP "test2";
+	print inP "(test3)(test3)";
+	print inP "(test4)";
+	print inP "[test5]", '[',']';
+	print "\n", stripOuter "(test)","\n";
+	while (42)
+	{
+		print "(expr)> ";
+		my $inp = <STDIN>;
+		$inp = substr $inp,0,length($inp) - 1;
+		print "evaluating $inp\n";
+		my $result = parseS($inp,[[$addRef,"+"],[$subRef,"-"],[$multRef,'*'],[$divRef,'/']],[],"(",")");
+		print "final result: $result\n";
 	}
 }
 1;
